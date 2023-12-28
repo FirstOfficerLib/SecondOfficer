ed5680d31b9838fc658c66668c5405dbc50ea063
Jerry Plankinton <jerry.plankinton@tylertech.com>
2023-12-28 11:07:54 -0600

    added projects

---
+++ SecondOfficer.Tests/SecondOfficerTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+#pragma warning disable VSTHRD200
+
+namespace SecondOfficer.Tests
+{
+    [TestFixture]
+    public abstract class SecondOfficerTest
+    {
+       
+    }
+}
